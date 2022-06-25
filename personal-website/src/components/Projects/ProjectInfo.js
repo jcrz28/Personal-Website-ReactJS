340630a93f20436715365f41cc9a74c62242ea92
John Leonard Cruz <cruzjohnleonard@gmail.com>
2022-06-25 00:41:07 -0500

    Better readability

--- personal-website/src/components/Projects/ProjectInfo.js
+++ personal-website/src/components/Projects/ProjectInfo.js
@@ -2,27 +2,30 @@ import {Accordion} from 'react-bootstrap';
 import classes from './ProjectInfo.module.css';
 
 const ProjectInfo = props => {
+    const technologyUsed = props.technology.map(technology => (
+        <label 
+            key = {technology}>
+            {technology} 
+            <span></span>
+        </label>
+    ));
+
+    const projectDescription = props.description.map(description => (
+        <li key = {description}>{description}</li>
+    ));
+
     return (
         <Accordion defaultkey={'key'} className={classes.accordion}>
             <Accordion.Item eventKey={'key'}>
                 <Accordion.Header>
                     <label className={classes.info}>More Info</label>
                     <div className={classes.technology}>
-                        {props.technology.map(technology => (
-                            <label 
-                                key = {technology}>
-                                {technology} 
-                                <span></span>
-                            </label>
-                        ))}
+                        {technologyUsed}
                     </div>
-
                 </Accordion.Header>
                 <Accordion.Body>
                     <ul>
-                        {props.description.map(description => (
-                            <li key = {description}>{description}</li>
-                        ))}
+                        {projectDescription}
                     </ul>
                 </Accordion.Body>
             </Accordion.Item>
--- personal-website/src/components/Projects/ProjectTitle.js
+++ personal-website/src/components/Projects/ProjectTitle.js
@@ -4,33 +4,26 @@ import { BsFillFileLockFill } from "react-icons/bs";
 import classes from './ProjectTitle.module.css';
 
 const ProjectTitle = props => {
+
+    const overlayTrigger = ['bottom'].map((placement) => (
+        <OverlayTrigger
+            key={placement}
+            placement={placement}
+            overlay={
+                <Tooltip id={`tooltip-${placement}`}>
+                GitHub Repository is <strong>Private</strong>.
+                </Tooltip>
+            }
+        >
+        <span> <BsFillFileLockFill/> </span>
+        </OverlayTrigger>
+    ));
+    
     return (
         <React.Fragment>
             <div className={classes.title}>
-                {
-                    props.public && 
-                    <header> {props.title} </header>
-                }
-
-                {
-                    !props.public && 
-                    <header> {props.title} 
-                        {['bottom'].map((placement) => (
-                            <OverlayTrigger
-                                key={placement}
-                                placement={placement}
-                                overlay={
-                                    <Tooltip id={`tooltip-${placement}`}>
-                                    GitHub Repository is <strong>Private</strong>.
-                                    </Tooltip>
-                                }
-                            >
-                            <span> <BsFillFileLockFill/> </span>
-                            </OverlayTrigger>
-                        ))}
-                    </header>
-                }
-
+                {props.public && <header> {props.title} </header>}
+                {!props.public &&  <header> {props.title} {overlayTrigger} </header> }
             </div>
         </React.Fragment>
     );
